0661eeb48a5d7e0f8e7c4aff6031eecfec6a5051
anton.fedorovsky <anton.fedorovsky@gmail.com>
2017-04-18 23:11:23 +0300

    hw-1

--- hw-typescript/hw-1/hw-1.js
+++ hw-typescript/hw-1/hw-1.js
@@ -1,6 +1,38 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Car = (function () {
-    function Car() {
+    function Car(mark, timeAcceleration, timeStop) {
+        this.mark = mark;
+        this.timeAcceleration = timeAcceleration;
+        this.timeStop = timeStop;
     }
+    Car.prototype.run = function () {
+        console.log("Time acceleration " + this.timeAcceleration);
+    };
+    Car.prototype.stop = function () {
+        console.log("Time stop " + this.timeStop);
+    };
     return Car;
 }());
+var LightCar = (function (_super) {
+    __extends(LightCar, _super);
+    function LightCar(mark, timeAcceleration, timeStop) {
+        var _this = _super.call(this, mark, timeAcceleration, timeStop) || this;
+        _this.wheels = 4;
+        return _this;
+    }
+    return LightCar;
+}(Car));
+var carOne = new LightCar('Toyota', 12, 8);
+carOne.run();
+carOne.stop();
+console.log(carOne);
 //# sourceMappingURL=hw-1.js.map
--- hw-typescript/hw-1/hw-1.ts
+++ hw-typescript/hw-1/hw-1.ts
@@ -1,13 +1,46 @@
 interface CarInterface {
-    mark: string;
-    wheels: number;
-    timeAcceleration: number;
-    timeBraking: number;
+  mark: string;
+  wheels: number;
+  timeAcceleration: number;
+  timeStop: number;
 
-    run();
-    stop();
+  run();
+  stop();
 }
 
 abstract class Car implements CarInterface {
+  public mark: string;
+  public timeAcceleration: number;
+  public timeStop: number;
+  public wheels: number;
 
+  constructor(mark:string, timeAcceleration:number, timeStop:number) {
+    this.mark = mark;
+    this.timeAcceleration = timeAcceleration;
+    this.timeStop = timeStop;
+  }
+
+  run() {
+    console.log(`Time acceleration ${this.timeAcceleration}`);
+  }
+
+  stop() {
+    console.log(`Time stop ${this.timeStop}`);
+  }
 }
+
+class LightCar extends Car {
+  wheels: number = 4;
+  mark: string;
+  timeAcceleration: number;
+  timeStop: number;
+  
+  constructor(mark:string, timeAcceleration:number, timeStop:number){
+    super(mark, timeAcceleration, timeStop);
+  }
+}
+
+let carOne = new LightCar('Toyota', 12 , 8);
+carOne.run();
+carOne.stop();
+console.log(carOne);
