e20aa15a432770ee6321f3dc965d13b9b6e53c48
Fedorovskyi <anton.fedorovsky@gmail.com>
2017-04-24 09:34:54 +0300

    hw-2

---
+++ simple-ng-app/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SimpleNgAppPage } from './app.po';
+
+describe('simple-ng-app App', function() {
+  let page: SimpleNgAppPage;
+
+  beforeEach(() => {
+    page = new SimpleNgAppPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
