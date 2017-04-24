0f0c3200aa286c1c08e8124d904381b2dc37eaab
anton.fedorovsky <anton.fedorovsky@gmail.com>
2017-04-24 21:57:30 +0300

    hw-2

--- simple-ng-app/src/app/app.module.ts
+++ simple-ng-app/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
-
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -13,7 +13,7 @@ import { ProductsComponent } from './pages/products/products.component';
 @NgModule({
   declarations: [
     AppComponent,
-    
+
     AboutUsComponent,
     ContactUsComponent,
     HomeComponent,
@@ -22,7 +22,30 @@ import { ProductsComponent } from './pages/products/products.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+      },
+      {
+        path: "home",
+        component: HomeComponent
+      },
+      {
+        path: "products",
+        component: ProductsComponent
+      },
+      {
+        path: "about-us",
+        component: AboutUsComponent
+      },
+      {
+        path: "contact-us",
+        component: ContactUsComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
