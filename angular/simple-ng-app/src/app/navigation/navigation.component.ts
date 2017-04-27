bb34d0d75066d7163b517e1dcd450a80db227c38
Ievhen Ganchyn <ievhenganchin@gmail.com>
2017-04-27 21:07:29 +0300

    Routes update #2

--- angular/simple-ng-app/src/app/navigation/navigation.component.ts
+++ angular/simple-ng-app/src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Home } from '../pages/home/home.component'
 import { appRoutes } from '../routes';
 
 @Component({
@@ -10,7 +11,8 @@ export class Navigation {
 
     routes: any;
 
-    constructor() {
+    constructor(private homeComponent: Home) {
+        // this.homeComponent = new Home();
         this.routes = appRoutes;
     }
 }
